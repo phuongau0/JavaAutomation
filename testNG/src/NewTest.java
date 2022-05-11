cacc073dcc2a8367cf9fc79671dc9152de322990
phuongau0 <71837528+phuongau0@users.noreply.github.com>
2022-05-11 20:38:41 +0700

    Add files via upload

---
+++ testNG/src/NewTest.java
@@ -0,0 +1,58 @@
+package testngpackage;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class NewTest {
+
+
+ 
+    public WebDriver driver=null ; 
+  //  public String baseUrl = "https://anhtester.com";
+
+  @BeforeTest
+  public void beforeTest() throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\Java\\chromedriver.exe");
+		 driver = new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+     // driver.navigate().to(baseUrl);
+    //  Thread.sleep(2000);
+
+   
+
+   Thread.sleep(2000);
+     // driver.quit();
+  }
+  @Test
+  public void Demo1() {
+	  driver.navigate().to("https://anhtester.");
+	  
+	  
+  }
+  
+  @Test
+  public void Demo2() {
+	  driver.navigate().to("https://google.com");
+	  
+  }
+
+  @Test
+  public void Demo3() {
+	  driver.navigate().to("https://youtube.com");
+	  
+  }
+  @AfterTest
+  public void AfterTest() throws InterruptedException {
+	  Thread.sleep(2000);
+	  driver.quit();
+}
+}
